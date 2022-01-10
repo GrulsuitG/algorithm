ae2d59571021d6ce565dc0c0aecb97d281e60228
grulsuitg <socros.bj@gmail.com>
2022-01-10 11:34:12 +0900

    ascii problem 1 solve

---
+++ String/Ascii1.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Ascii1 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int s = br.read();
+        System.out.println(s);
+
+    }
+}
