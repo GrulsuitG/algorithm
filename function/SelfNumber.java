f57fe438218df21fae6297a3eb05670fbfd3a85d
grulsuitg <socros.bj@gmail.com>
2022-01-10 10:53:16 +0900

    SelfNumber solve

---
+++ function/SelfNumber.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class SelfNumber{
+
+    int d(int num){
+        int sum = num;
+        while( num != 0){
+            sum += num%10;
+            num /= 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) throws IOException {
+        int res;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] arr = new int[10001];
+        SelfNumber s = new SelfNumber();
+
+        for(int i = 1; i<10000; i++){
+            res = s.d(i);
+            try {
+                arr[res] = 1;
+            }catch(Exception e){
+                continue;
+            }
+        }
+
+        for(int i = 1; i<10000; i++){
+            if (arr[i] == 0)
+//                bw.write(Integer.toString(i));
+                System.out.println(i);
+        }
+    }
+
+
+}
