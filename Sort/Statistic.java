2e11361ad95d2d59faa84717451452a98ec9b16a
grulsuitg <socros.bj@gmail.com>
2022-02-15 18:25:42 +0900

    Statistic problem solve

--- Sort/Statistic.java
+++ Sort/Statistic.java
@@ -5,8 +5,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class Statistic {
     public static void main(String[] args) throws IOException {
