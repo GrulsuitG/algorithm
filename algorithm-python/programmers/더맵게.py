7efc2603a7992df9c004d6b77694fa4c97297047
GrulsuitG <95327051+GrulsuitG@users.noreply.github.com>
2022-05-21 15:21:02 +0900

    22.05.21 problem solve

--- algorithm-python/programmers/더맵게.py
+++ algorithm-python/programmers/더맵게.py
@@ -1,5 +1,11 @@
 import heapq
 
-a = heapq.heapify([1,2,3])
-
-print(a)
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    while scoville[0] < K:
+        if len(scoville) < 2:
+            return -1
+        heapq.heappush(scoville, heapq.heappop(scoville) + heapq.heappop(scoville) * 2)
+        answer += 1
+    return answer
