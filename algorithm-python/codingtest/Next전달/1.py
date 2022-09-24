7d644bf8bd8c9c6eda6af5ec6dd54e0d3aabbb71
GrulsuitG <95327051+GrulsuitG@users.noreply.github.com>
2022-09-24 12:43:58 +0900

    Delete algorithm-python/codingtest/Next전달 directory

--- algorithm-python/codingtest/Next전달/1.py
+++
@@ -1,9 +0,0 @@
-def solution(s):
-    answer = -1
-    for i in range(10):
-        sub = str(i) * 3
-        if sub in s:
-            answer = int(sub)
-    return answer
-
-print(solution("111999333"))
